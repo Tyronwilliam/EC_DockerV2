d80caccfa1564b9c6caee9bf51a2f179cf240e12
Tyronwilliam <tyronwillianchanu@gmail.com>
2023-03-11 18:32:06 +0100

    first commit

---
+++ back/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const router = express.Router();
+
+// Databases
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST || "localhost",
+  port: 3306,
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "mystrongpassword",
+  database: process.env.MYSQL_DATABASE || "ecomv2",
+});
+
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM `test`", function (error, results, fields) {
+    if (error) throw error;
+
+    if (results.length > 0) {
+      res.status(200).send(results);
+    } else {
+      res.status(404).send("No data found");
+    }
+  });
+});
+
+app.get("/test", (req, res) => {
+  connection.query("SELECT * FROM `Houra`", function (error, results, fields) {
+    if (error) throw error;
+
+    if (results.length > 0) {
+      res.status(200).send(results);
+    } else {
+      res.status(404).send("No data found");
+    }
+  });
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Back is up on port ${port}`);
+});
